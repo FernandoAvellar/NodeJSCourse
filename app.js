8e1503e938797b8aef68f81a8023beff70d25f02
Fernando Avellar <fernando.avellar@inatel.br>
2017-07-13 17:17:29 -0300

    Extraindo as rotas

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
-var express = require('express')
-var app = express()
+var app = require('./config/express')()
+var rotasProdutos = require('./app/routes/produtos')(app)
 
-app.listen(3000, function() {
+app.listen(3000, function () {
     console.log("Servidor rodando")
-})
-
-app.set('view engine','ejs')
-
-app.get('/produtos', function(req, res) {
-    res.render("produtos/lista")
 })
