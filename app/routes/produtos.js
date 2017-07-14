bd73e215f39e65944ee720ccffebce14c064856e
Fernando Avellar <fernando.avellar@inatel.br>
2017-07-14 15:44:29 -0300

    Iniciando com o banco de dados

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,5 +1,16 @@
 module.exports = function (app) {
     app.get('/produtos', function (req, res) {
-    res.render("produtos/lista")
+        var mysql = require('mysql')
+        var connection = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: 'root',
+            database: 'casadocodigo_nodejs'
+        })
+
+        connection.query('select * from produtos', function(err, results) {
+            res.send(results)
+        })
+        connection.end()
     })
 }
